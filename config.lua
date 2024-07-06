072c8aa4804c2b5363e457133904c8d85a9e0ca3
RexshackGaming <rexshackuk@gmail.com>
2024-07-06 20:56:30 +0100

    crafting lead bar

--- config.lua
+++ config.lua
@@ -239,6 +239,16 @@ Config.BlacksmithCrafting = {
         crafttime = 30000,
         ingredients = { 
             [1] = { item = 'coal', amount = 10 },
+            [2] = { item = 'leadore', amount = 10 },
+        },
+        receive = 'leadbar',
+        giveamount = 1
+    },
+    {
+        category = 'Bars',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 10 },
             [2] = { item = 'copperore', amount = 10 },
         },
         receive = 'copperbar',
